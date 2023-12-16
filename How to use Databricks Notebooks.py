daa411a14eaafda7ceb3c382b92db9197fac574c
viratarya <viratarya>
2023-12-16 04:40:32 +0000

    initail commit
    initial commit to test integrations

---
+++ How to use Databricks Notebooks.py
@@ -0,0 +1,23 @@
+# Databricks notebook source
+
+print('xyz')
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC # Header
+# MAGIC ## Sub header
+# MAGIC ### sub sub header
+
+# COMMAND ----------
+
+print("Hello World")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from samples.nyctaxi.trips;
+
+# COMMAND ----------
+
+
