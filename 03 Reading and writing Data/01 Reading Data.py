82e002f4dd0122d4ad9128f457406f6de2a2a036
viratarya <viratarya>
2023-12-19 01:17:23 +0000

    updated reading

--- 03 Reading and writing Data/01 Reading Data.py
+++ 03 Reading and writing Data/01 Reading Data.py
@@ -51,6 +51,76 @@ countries_df = spark.read.options(header=True,inferSchema=True).csv('/FileStore/
 
 # COMMAND ----------
 
+from pyspark.sql.types import IntegerType, StringType, DoubleType, StructType, StructField
+
+
+countries_schema= StructType([StructField('COUNTRY_ID', IntegerType(), False),
+            StructField('NAME', StringType(), False),
+            StructField('NATIONALITY', StringType(), False),
+            StructField('COUNTRY_CODE', StringType(), False),
+            StructField('ISO_ALPHA2', StringType(), False),
+            StructField('CAPITAL', StringType(), False),
+            StructField('POPULATION', IntegerType(), False),
+            StructField('AREA_KM2', DoubleType(), False),
+            StructField('REGION_ID', IntegerType(), True),
+            StructField('SUB_REGION_ID', IntegerType(), True),
+            StructField('INTERMEDIATE_REGION_ID',IntegerType(), True),
+            StructField('ORGANIZATION_REGION_ID',IntegerType(), True)])
+
+# COMMAND ----------
+
+countries_df = spark.read.options(header=True).schema(countries_schema).csv('/FileStore/tables/countries.csv')
+
+# COMMAND ----------
+
+countries_sl_json = spark.read.json('dbfs:/FileStore/tables/countries_single_line.json')
+
+# COMMAND ----------
+
+display(countries_sl_json)
+
+# COMMAND ----------
+
+countries_ml_json = spark.read.options(multiLine = True).json('dbfs:/FileStore/tables/countries_multi_line.json')
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+display(countries_ml_json)
+
+# COMMAND ----------
+
+countries_txt = spark.read.csv('dbfs:/FileStore/tables/countries.txt',header = True, sep ='\t')
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
 
 
 # COMMAND ----------
